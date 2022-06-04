c0bc91df68d865a4256ee71c53083c29bec213e6
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-06-04 11:26:30 +0200

    refactor of code. All express calls are now in startup/routes

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const express = require('express');
 
 module.exports = function(app){
+
+    app.use(express.static(path.join(__dirname, "..")));
+
     app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, '../index.html'));
       });
